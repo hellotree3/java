75e75f15bbe6f652a1670528fe1b54158e67d500
hellotree <liuxiulian376@163.com>
2017-09-16 00:56:11 +0800

    my first branch file

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Created by hellotree on 2017/9/16.
+ *
+ * @author by 邻居的耳朵
+ * @Begin 2017/9/16.
+ */
+public class Hello {
+    public static void main(String args[]){
+         System.out.println("我的第一个git文件");
+        System.out.println("我的branch");
+    }
+}
